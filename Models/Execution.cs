507145a5a19b52bde5e5b80887d8e4b7c5d6eab5
Aziz Mahmoudy <aziz.mahmoudy@hotmail.com>
2020-09-15 18:02:32 +0200

    changed execution model

--- Models/Execution.cs
+++ Models/Execution.cs
@@ -3,5 +3,8 @@ public class Execution
 {
     [Key]
     public long Id { get; set; }
-    public bool IsComplete { get; set; }
+
+    public string TestcaseId { get; set; }
+    
+    public string Status { get; set; }
 }
