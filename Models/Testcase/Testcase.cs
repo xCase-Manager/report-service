27eafdf69c3f69257de3a6bd198a1e334b98b810
Aziz Mahmoudy <aziz.mahmoudy@hotmail.com>
2020-10-03 11:18:30 +0200

    added DB entity properties

--- Models/Testcase/Testcase.cs
+++ Models/Testcase/Testcase.cs
@@ -10,10 +10,15 @@ namespace Models.Testcase
         public string _Id { get; set; }
 
         [BsonElement("title")]
-        public string TestcaseName { get; set; }
+        public string Name { get; set; }
 
+        [BsonElement("description")]
         public string Description { get; set; }
 
+        [BsonElement("projectId")]
         public string ProjectId { get; set; }
+
+        [BsonElement("id")]
+        public int Id { get; set; }
     }
 }
