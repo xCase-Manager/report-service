1fbe9080163071bb99c0ce3a54780d26c1c8e21c
Aziz Mahmoudy <aziz.mahmoudy@hotmail.com>
2020-10-08 17:10:54 +0200

    added project collection properties

--- Models/DBSettings.cs
+++ Models/DBSettings.cs
@@ -2,6 +2,7 @@ namespace XCaseManager.ReportService.Models
 {
     public class DBSettings : IDBSettings
     {
+        public string ProjectCollectionName { get; set; }
         public string TestcaseCollectionName { get; set; }
         public string ConnectionString { get; set; }
         public string DatabaseName { get; set; }
@@ -9,6 +10,7 @@ namespace XCaseManager.ReportService.Models
 
     public interface IDBSettings
     {
+        string ProjectCollectionName { get; set; }
         string TestcaseCollectionName { get; set; }
         string ConnectionString { get; set; }
         string DatabaseName { get; set; }
